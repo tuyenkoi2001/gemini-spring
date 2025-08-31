97b881059efdd32630e3850fc7d6733b7c1ab8be
tuyenkoi2001 <tuyenkoi2020@gmail.com>
2025-08-31 22:17:31 +0700

    feat: Add Dart code for API POST request

--- src/main/java/com/example/gemini_ai/controller/ExerciseController.java
+++ src/main/java/com/example/gemini_ai/controller/ExerciseController.java
@@ -1,5 +1,6 @@
 package com.example.gemini_ai.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class ExerciseController {
 	}
 	
 	@PostMapping("/api/v1/exercises")
+	@CrossOrigin(origins = "http://localhost:8080")
 	public String GetSingleExerciseWithConfig (@RequestBody ExerciseConfig config) {
 		ExerciseBuilder promptBuilder = new ExerciseBuilder();
 
